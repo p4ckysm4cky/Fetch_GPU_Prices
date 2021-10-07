e3cad42b89e4d6df38f51684620300b521282d1f
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-10-08 11:47:00 +1300

    It seems to work, but I need to fix formating

--- scraping/main.py
+++ scraping/main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def pbtech_scrape(url_array):
     """
     Takes in an array of url's and returns an array of tuples containing
@@ -10,33 +11,39 @@ def pbtech_scrape(url_array):
     for url in url_array:
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
-        # finding item names
+        # finding name of items
         item_names = soup.find_all("div", class_="item_short_name")
-        # finding item prices
+        # finding price of items
         prices = soup.find_all("span", class_="price_full")
         # this needs to be done, because there are multiple prices:
         # odd index = GST included, even index = GST not included
-        prices_formatted = [prices[i] for i in range(len(prices)) if i % 2 != 0]
+        prices_formatted = [prices[i]
+                            for i in range(len(prices)) if i % 2 != 0]
         for i in range(len(item_names)):
             item_name = (item_names[i].span.text).split(",")[0]
             price = prices_formatted[i].text
             pbtech_items.append((item_name, price))
-        return pbtech_items
+    return pbtech_items
 
 
-# url = "https://www.pbtech.co.nz/category/components/video-cards/nvidia-desktop-graphics-cards?o=lowest_price"
-# page = requests.get(url)
-# soup = BeautifulSoup(page.content, 'html.parser')
-# nvidia_gpus = soup.find_all("div", class_="item_short_name")
+def print_items(item_array):
+    """
+    Takes in an array of (item_name, price) tuple
+    and prints a formatted text of the item. 
+    """
+    for name, price in item_array:
+        print(f"{name:80}| {price}")
 
-# prices = soup.find_all("span", class_="price_full")
-# prices_formatted = [prices[i] for i in range(len(prices)) if i % 2 != 0]
 
-# for i in range(len(nvidia_gpus)):
-#     gpu_name = (nvidia_gpus[i].span.text).split(",")[0]
-#     # print(f"{(nvidia_gpus[i].span.text).split(",")[0] :30} {prices_formatted[i].text}")
-#     print(f"{gpu_name:80}| {prices_formatted[i].text}")
+def main():
+    gpu_url = ["https://www.pbtech.co.nz/category/components/video-cards/shop-all?pg=1#sort_group_form",
+               "https://www.pbtech.co.nz/category/components/video-cards/shop-all?pg=2#sort_group_form",
+               "https://www.pbtech.co.nz/category/components/video-cards/shop-all?pg=3#sort_group_form",
+               "https://www.pbtech.co.nz/category/components/video-cards/shop-all?pg=4#sort_group_form"]
+    gpu_array = pbtech_scrape(gpu_url)
+    print("="*120)
+    print_items(gpu_array)
+
+
 if __name__ == "__main__":
-    url = ["https://www.pbtech.co.nz/category/components/video-cards/nvidia-desktop-graphics-cards?o=lowest_price"]
-    gpu_array = pbtech_scrape(url)
-    print(gpu_array)
+    main()
